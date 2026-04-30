7797cbd5af5d1a7152b02cbbcb9f551ed08c230e
poconakara95 <poconakara95@gmail.com>
2026-04-30 15:31:58 +0800

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -16,7 +16,7 @@ exports.handler = async function(event) {
     const result = await new Promise((resolve, reject) => {
       const req = https.request({
         hostname: 'generativelanguage.googleapis.com',
-        path: '/v1beta/models/gemini-1.5-flash-latest:generateContent?key=' + apiKey,
+        path: '/v1/models/gemini-1.5-flash-latest:generateContent?key=' + apiKey,
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',
